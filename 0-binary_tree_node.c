c50f14d44e497bb694f9320448e2682a7bf0381a
AnoR <94970934+the4thmusketeerr@users.noreply.github.com>
2023-08-03 22:00:21 +0000

    Create 0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+typedef struct binary_tree_s {
+    int value;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+    binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+    if (new_node == NULL) {
+        return NULL; // Return NULL on failure
+    }
+
+    new_node->value = value;
+    new_node->parent = parent;
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    return new_node;
+}
